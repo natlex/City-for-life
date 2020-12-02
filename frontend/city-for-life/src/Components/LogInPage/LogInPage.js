4028d044e17c5e9e3512f725a19bd06582de8c58
Sasha <ermakovaas7@gmail.com>
2020-12-02 21:48:11 +0300

    created app, added routing +

---
+++ frontend/city-for-life/src/Components/LogInPage/LogInPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './LogInPage.css';
+
+const LogInPage = () => {
+  return (
+    <div className="login-page">
+      Залогиниться можно тут
+    </div>
+  );
+}
+
+export default LogInPage;
---
+++ frontend/city-for-life/src/Components/MapPage/MapPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './MapPage.css';
+
+const MapPage = () => {
+  return (
+    <div className="map-page">
+      Страница с картой
+    </div>
+  );
+}
+
+export default MapPage;
