7b03f4985b4e365c963042e1426d9b233b0ca321
Sasha <ermakovaas7@gmail.com>
2020-12-10 10:13:28 +0300

    changed dir inside frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header'
 import LogInPage from './Components/LogInPage/LogInPage'
 import MapPage from './Components/MapPage/MapPage';
-import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Test from './Components/Test1/Test1';
+
+
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="app">
+      <div className='app'>
         <Header />
         <Navbar />
         <div className='app-content'>
--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
+import { NavLink } from 'react-router-dom';
 
 import './Navbar.css'
 
